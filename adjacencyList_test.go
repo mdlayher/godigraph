f8000416694226bb8e631057b9745efe713e9630
Matt Layher <mdlayher@gmail.com>
2014-03-21 22:13:51 -0400

    adjacencyList_test, add TestSearch

--- adjacencyList_test.go
+++ adjacencyList_test.go
@@ -26,3 +26,46 @@ func TestAdjacent(t *testing.T) {
 		}
 	}
 }
+
+// TestSearch verifies that the Search method is working properly
+func TestSearch(t *testing.T) {
+	log.Println("TestSearch()")
+
+	// Create an adjacency list
+	adjList := AdjacencyList{list.New()}
+
+	// Generate some adjacent vertices for the list
+	elements := []Vertex{1, 2, 3}
+	for _, e := range elements {
+		adjList.list.PushBack(e)
+	}
+
+	// Create a table of tests and expected element results
+	var tests = []struct {
+		vertex interface{}
+		result interface{}
+	}{
+		// Existing vertices
+		{1, 1},
+		{2, 2},
+		{3, 3},
+		// Non-existant vertices
+		{4, nil},
+		{5, nil},
+		{6, nil},
+	}
+
+	// Iterate test table, check results
+	for _, test := range tests {
+		// Check for element
+		element := adjList.Search(test.vertex)
+
+		// If element is nil and it should not be, test fails
+		if element == nil && test.result != nil {
+			t.Fatalf("adjList.Search(%d) - unexpected result: nil", test.vertex)
+		} else if element != nil && element.Value != test.result {
+			// If element is not nil, but is value does not match, test fails
+			t.Fatalf("adjList.Search(%d) - unexpected result: %v", test.vertex, element.Value)
+		}
+	}
+}
